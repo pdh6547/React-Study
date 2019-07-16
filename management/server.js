e2d17603dcfe3ef4f0a89d917c9a32e33a278987
pdh6547 <pdh6547@gmail.com>
2019-07-16 20:31:58 +0900

    Implement Customer Add Module & Customer List Refresh Module

--- management/server.js
+++ management/server.js
@@ -9,20 +9,45 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const data = fs.readFileSync('./database.json');
 const conf = JSON.parse(data);
-const mysql = require('mysql'); 
+const mysql = require('mysql');
 
 const connection = mysql.createConnection({
     host: conf.host,
-    user : conf.user,
-    password : conf.password,
+    user: conf.user,
+    password: conf.password,
     port: conf.port,
     database: conf.database
 });
 connection.connect();
 
+const multer = require('multer');
+const upload = multer({ dest: './upload' })
+
 app.get('/api/customers', (req, res) => {
     connection.query(
         "select * from customer",
+        (err, rows, fields) => {
+            res.send(rows);
+        }
+    );
+});
+
+app.use('/image', express.static('./upload'));
+
+app.post('/api/customers', upload.single('image'), (req, res) => {
+    let sql = 'insert into customer values (null, ?, ?, ?, ?, ?)';
+    let image = '/image/' + req.file.filename;
+    let name = req.body.name;
+    let birthday = req.body.birthday;
+    let gender = req.body.gender;
+    let job = req.body.job;
+    console.log(name);
+    console.log(image);
+    console.log(birthday);
+    console.log(gender);
+    console.log(job);
+    let params = [image, name, birthday, gender, job];
+    connection.query(sql, params,
         (err, rows, fields) => {
             res.send(rows);
         }
